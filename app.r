dbc66b8cfb4e8a1e4a194435c242ab2e63672c05
abarons <54209778+abarons@users.noreply.github.com>
2019-08-20 14:15:49 +0200

    V0.2 Alpha

--- app.r
+++ app.r
@@ -1,10 +1,23 @@
+
+
+# Libraries ---------------------------------------------------------------
+
 library(shiny)
 library(shinydashboard)
-library(shinyWidgets)
+library(shinycssloaders)
+library(DT)
+library(mclust)
+library(dbscan)
 
+
+# Support Functions -------------------------------------------------------
 source("clustercore.r")
 
-dashboardHeader<-dashboardHeader(title = "Clustering App",titleWidth="300",tags$li(a(href = 'https://github.com/abarons/clusterApp',
+
+
+# UI ----------------------------------------------------------------------
+
+dashboardHeader<-dashboardHeader(title = "Cluster App",titleWidth="300",tags$li(a(href = 'https://github.com/abarons/clusterApp',
                                                                     img(src = "https://image.flaticon.com/icons/png/128/1051/1051326.png",
                                                                         title = "GitHub Source Code", height = "30px"),
                                                                     style = "padding-top:10px; padding-bottom:10px;"),
@@ -49,28 +62,63 @@ dashboardSidebar<-  dashboardSidebar( width = 300,
                                         
                                         # Horizontal line ----
                                         tags$hr(),
+
                                         
                                         # KMEANS Menu Item
-                                        
-                                        menuItem("KMEANS", icon = icon("bar-chart-o"),
-                                                     textInput("clusternumber", "Number Of Cluster", value = "2"),
+                                        menuItem("KMEANS", icon = icon("wrench"),
+                                                     textInput("clusternumber", "Number Of Clusters", value = "2"),
+                                                     checkboxInput("normalizeKMEANS", "Min - Max Normalization", FALSE),
                                                      actionButton("kmeans", "Cluster with KMEANS"),
-                                                     p("Click here to execute KMEANS clustering algorithm.")
+                                                     p("Click to execute KMEANS clustering algorithm.")
                                                      ),
              
                                         
                                         # AGNES Menu Items
-                                        menuItem("AGNES",icon=icon("bar-chart-o"),
+                                        menuItem("AGNES",icon=icon("wrench"),
                                                   selectInput("linkageFunction", "Select a Linkage Criterion",
                                                     c("ward.D","ward.D2","single","complete","average","mcquitty", "median", "centroid")
                                                     ,width = "100%"),
                                                   selectInput("distance", "Select a Distance Metric",
                                                     c("euclidean", "maximum", "manhattan", "canberra", "binary","minkowski")
                                                     ,width = "100%"),
+                                                 textInput("clustertree", "Number Of Clusters", value = "2"),
+                                                 checkboxInput("normalizeAGNES", "Min - Max Normalization", FALSE),
                                                   actionButton("hclust", "Cluster with AGNES"),
                                                   p("Click to execute AGNES clustering algorithm.")
                                         
-                                                )
+                                                ),
+                                        
+                                        
+                                        # DBSCAN Menu Items
+                                        menuItem("DBSCAN",icon=icon("wrench"),
+                                                 textInput("eps", "Epsilon", value = "0.1"),
+                                                 textInput("minpts", "Min Points", value = "2"),
+                                                 checkboxInput("normalizeDBSCAN", "Min - Max Normalization", FALSE),
+                                                 actionButton("dbscan", "Cluster with DBSCAN"),
+                                                 p("Click to execute DBSCAN clustering algorithm.")
+                                                 
+                                        ),
+                                        
+                                        
+                                        # EMMG Menu Items
+                                        menuItem("EMMG",icon=icon("wrench"),
+                                                 textInput("n", "Maximum Simulated Mixtures", value = "4"),
+                                                 checkboxInput("normalizeEMMG", "Min - Max Normalization", FALSE),
+                                                 actionButton("emmg", "Cluster with EMMG"),
+                                                 p("Click to execute EMMG clustering algorithm.")
+                                                 
+                                        ),
+                                        
+                                        
+                                        # Download Menu Items
+                                        menuItem("Download Results",icon=icon("fas fa-download"),
+                                                 # Horizontal line ----
+                                                 tags$hr(),
+                                                 p("A Download link will appear as soon as"),
+                                                 p("one of the algorithms is activated."),
+                                                 uiOutput("download")
+                                        
+                                        )
                                         )
 )
 
@@ -79,11 +127,14 @@ dashboardSidebar<-  dashboardSidebar( width = 300,
 
 
 
-dashboardBody<- dashboardBody(
+dashboardBody<- dashboardBody(tags$head(tags$style("section.content { overflow-y: hidden; }
+                                                   .content-wrapper, .right-side {background-color:  #f8f9f9;}")),
+                              
+                              #First Text Box
                               box(
-                                title = "Dataset Top 5", solidHeader = TRUE,
+                                title = "How To Use Cluster App", solidHeader = TRUE,
                                 collapsible = TRUE,
-                                tableOutput("contents"),
+                                verbatimTextOutput("urlText"),
                                 width=12,
                                 background = "purple"
                               ),
@@ -95,14 +146,62 @@ dashboardBody<- dashboardBody(
                               # Horizontal line ----
                               tags$hr(),
                               
+                              
+                              #Dataset Data Table
+                              box(
+                                title = "Dataset", solidHeader = TRUE,
+                                collapsible = TRUE,
+                                DT::dataTableOutput("contents"),style = "height:300px; overflow-y: scroll;overflow-x: scroll;",
+                                width=6,
+                                status = "primary"
+                              ),
+                              
+                              
+                              #Summary Text Box
+                              box(
+                                title = "Summary Dataset", solidHeader = TRUE,
+                                collapsible = TRUE,
+                                verbatimTextOutput("summary"),style = "height:300px; overflow-y: scroll;overflow-x: scroll;",
+                                width=6,
+                                status = "primary"
+                              ),
+                              
+                              # Horizontal line ----
+                              tags$hr(),
+                              # Horizontal line ----
+                              tags$hr(),
+                              # Horizontal line ----
+                              tags$hr(),
+                              
+                              
+                              #Cluster Plot
                               box(
                                 title = "Cluster Plot", solidHeader = TRUE,
                                 collapsible = TRUE,
-                                plotOutput('plot',width="100%"),
+                                plotOutput('plot',width="100%") %>% withSpinner(color="#0dc5c1") ,
                                 width=12,
-                                background = "black"
+                                status = "primary"
+                              ),
+                              
+                              # Horizontal line ----
+                              tags$hr(),
+                              # Horizontal line ----
+                              tags$hr(),
+                              # Horizontal line ----
+                              tags$hr(),
+                              
+                              
+                              #Summary Text Box
+                              box(
+                                title = "Cluster Summary", solidHeader = TRUE,
+                                collapsible = TRUE,
+                                verbatimTextOutput("summarycluster"),style = "height:300px; overflow-y: scroll;overflow-x: scroll;",
+                                width=12,
+                                status = "primary"
                               )
+                             
 )
+
 
 
 
@@ -115,83 +214,160 @@ ui <- dashboardPage(skin = "purple",
     
   
  
+
+# Server ------------------------------------------------------------------
+
+server <- function(input, output,session) {
+  
+  output$download <- renderUI({
+    if(input$kmeans==1 || input$hclust==1 || input$dbscan==1 || input$emmg==1) {
+      downloadButton('downloadData', 'Download Clustering Results')
+    }
+  })
   
   
-server <- function(input, output) {
-  
+  output$urlText <- renderText({
+    return("Try starting one of the algorithms positioned on the left sidebar, using Iris Dataset.\nOr, upload a csv file. Cluster App will automatically find the features to perform clustering\nThen,go to download panel to save results.")
+  })
     
-    output$contents <- renderTable({
-      
-      if(is.null(input$file1)){
-        return(head(datasets::iris,5))
+  
+  dataset <- reactive({
+
+    if(is.null(input$file1)){
+      return(datasets::iris)
+    }
+    
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, head of that data file by default,
+    # or all rows if selected, will be shown.
+    
+    req(input$file1)
+    
+    # when reading semicolon separated files,
+    # having a comma separator causes `read.csv` to error
+    tryCatch(
+      {
+        df <- read.csv(input$file1$datapath,
+                       header = input$header,
+                       sep = input$sep,
+                       dec=input$decimal
+        )
+      },
+      error = function(e) {
+        # return a safeError if a parsing error occurs
+        stop(safeError(e))
       }
-      
-      # input$file1 will be NULL initially. After the user selects
-      # and uploads a file, head of that data file by default,
-      # or all rows if selected, will be shown.
-      
-      req(input$file1)
-      
-      # when reading semicolon separated files,
-      # having a comma separator causes `read.csv` to error
-      tryCatch(
-        {
-          df <- read.csv(input$file1$datapath,
-                         header = input$header,
-                         sep = input$sep,
-                         dec=input$decimal
-                         )
-        },
-        error = function(e) {
-          # return a safeError if a parsing error occurs
-          stop(safeError(e))
-        }
-      )
-      return(head(df,5))
-      
+    )
+    return(df)
+    
+  })
+  
+    output$contents <- renderDataTable({
+      return(dataset())
     })
+    
+    
+    
+    output$summary  <- renderPrint({ 
+      return(summary(dataset()))
+    })
+    
     
     kmeans <- eventReactive(input$kmeans, {
-      
-      if(is.null(input$file1)){
-        return(kmeans.output(datasets::iris,input$clusternumber))
-      }
-      
-      df <- read.csv(input$file1$datapath,
-                     header = input$header,
-                     sep = input$sep,
-                     dec = input$decimal,
-                     stringsAsFactors = FALSE)
-      return(kmeans.output(df,input$clusternumber))
+      return(kmeans.output(dataset(),input$clusternumber,input$normalizeKMEANS))
     })
-    
-    
-    
-    hclust <- eventReactive(input$hclust, {
-      
-      if(is.null(input$file1)){
-        return(hclust.output(datasets::iris,input$linkageFunction,input$distance,input$clusternumber))
-      }
-
-      df <- read.csv(input$file1$datapath,
-                     header = input$header,
-                     sep = input$sep,
-                     dec = input$decimal,
-                     stringsAsFactors = FALSE)
-      return(hclust.output(df,input$linkageFunction,input$distance,input$clusternumber))
-    })
-    
-    observeEvent(input$hclust,output$plot <- renderPlot({
-      hclust.plot(hclust())
-    }))
-    
-    observeEvent(input$kmeans,output$plot <- renderPlot({
-    kmeans.plot(kmeans())
-   }))
- 
- 
-} 
     
   
+    hclust <- eventReactive(input$hclust, {
+      return(hclust.output(dataset(),input$linkageFunction,input$distance,input$clustertree,input$normalizeAGNES))
+    })
+    
+    
+    
+    dbscan <- eventReactive(input$dbscan, {
+      return(dbscan.output(dataset(),input$eps,input$minpts,input$normalizeDBSCAN))
+    })
+    
+    emmg <- eventReactive(input$emmg, {
+      return(emmg.output(dataset(),input$n,input$normalizeEMMG))
+    })
+    
+    observeEvent(input$hclust,{
+      output$plot <- renderPlot({
+      hclust.plot(hclust())
+      })
+      output$summarycluster <- renderPrint({
+      hclust.summary(hclust())
+    })
+     output$downloadData <- downloadHandler(
+        filename = function() {
+          paste('HCLUST_ClusteringResults.csv', sep='')
+        },
+        content = function(con) {
+          write.csv(hclust()$output, con,row.names = FALSE,quote=FALSE)
+        }
+      )
+  })
+    
+    observeEvent(input$kmeans,{
+      output$plot <- renderPlot({
+        kmeans.plot(kmeans())
+      })
+      output$summarycluster <- renderPrint({
+        kmeans.summary(kmeans())
+      })
+      output$downloadData <- downloadHandler(
+        filename = function() {
+          paste('KMEANS_ClusteringResults.csv', sep='')
+        },
+        content = function(con) {
+          write.csv(kmeans()$output, con,row.names = FALSE,quote=FALSE)
+        }
+      )
+    })
+    
+    observeEvent(input$dbscan,{
+      output$plot <- renderPlot({
+        dbscan.plot(dbscan())
+      })
+      output$summarycluster <- renderPrint({
+        dbscan.summary(dbscan())
+      })
+      output$downloadData <- downloadHandler(
+        filename = function() {
+          paste('DBSCAN_ClusteringResults.csv', sep='')
+        },
+        content = function(con) {
+          write.csv(dbscan()$output, con,row.names = FALSE,quote=FALSE)
+        }
+      )
+    })
+    
+    
+    observeEvent(input$emmg,{
+      output$plot <- renderPlot({
+        emmg.plot(emmg())
+      })
+      output$summarycluster <- renderPrint({
+        emmg.summary(emmg())
+      })
+      output$downloadData <- downloadHandler(
+        filename = function() {
+          paste('EMMG_ClusteringResults.csv', sep='')
+        },
+        content = function(con) {
+          write.csv(emmg()$output, con,row.names = FALSE,quote=FALSE)
+        }
+      )
+    })
+    
+   
+ 
+} 
+
+
+
+# Run App -----------------------------------------------------------------
+
 shinyApp(ui, server)
   
--- clustercore.r
+++ clustercore.r
@@ -1,49 +1,55 @@
-
-
-# Kmeans ------------------------------------------------------------------
-
-kmeans.output<-function(df_dataset_original,k){
-
-#PreProcess Data
-df_dataset_original<-na.omit(df_dataset_original)
-df_dataset_original<-apply(df_dataset_original,2,as.numeric)
-df_dataset<-df_dataset_original[,apply(df_dataset_original, 2, function(x) !any(is.na(x)))]
-
-
-#If ncol>2, then use PC for clustering
-if(ncol(df_dataset)>2){
- mat_dataset<-prcomp(df_dataset)$x[,1:2]
-}else{
- mat_dataset<-df_dataset
+# Pre Process Data --------------------------------------------------------
+#Remove NA Rows,convert all columns in numeric and remove not numeric columns
+#If Normalize == TRUE, then process the Min-Max Normalization
+preProcessData<-function(df_dataset,normalize){
+  df_dataset<-na.omit(df_dataset)
+  df_dataset<-apply(df_dataset,2,as.numeric)
+  df_dataset<-df_dataset[,apply(df_dataset, 2, function(x) !any(is.na(x)))]
+  if(normalize){
+    df_dataset<-apply(df_dataset,2,function(x){(x-min(x))/(max(x)-min(x))})
+  }
+  return(df_dataset)
 }
 
-model_km<-kmeans(mat_dataset
-                 ,centers=k
-                 )
-df_output<-data.frame(df_dataset_original,Cluster=model_km$cluster)
-list_output<-list(df_output,mat_dataset,model_km$cluster,model_km$centers)
-names(list_output)<-c("output","input","labels","centers")
-return(list_output)
+
+
+# KMEANS ------------------------------------------------------------------
+
+#Perform a KMEANS clusterization
+kmeans.output<-function(df_dataset_original,k,normalize){
+  df_dataset<-preProcessData(df_dataset_original,normalize)
+  model_km<-kmeans(df_dataset
+                   ,centers=k
+  )
+  df_output<-data.frame(df_dataset_original,Cluster=model_km$cluster)
+  list_output<-list(df_output,df_dataset,model_km$cluster,model_km)
+  names(list_output)<-c("output","input","labels","model")
+  return(list_output)
 }
 
+
+
+#Plot KMEANS results cluster, if columns>2, then plot Principal Components
 kmeans.plot<-function(list_output){
-plot(list_output$input,col=list_output$labels,pch=19)
-points(list_output$centers,col=1:5,pch=8,cex=3)
-return(invisible())
+  if(ncol(list_output$input)>2){
+    list_output$input<-prcomp(list_output$input)$x[,1:2]
+  }
+  plot(list_output$input,col=list_output$labels,pch=19)
+  return(invisible())
+}
+
+#Summary
+kmeans.summary<-function(list_output){
+  return(list_output$model)
 }
 
 
 
 # HCLUST ------------------------------------------------------------------
 
-hclust.output<-function(df_dataset_original,linkagefun,distance,k){
-  
-  #PreProcess Data
-  df_dataset_original<-na.omit(df_dataset_original)
-  df_dataset_original<-apply(df_dataset_original,2,as.numeric)
-  df_dataset<-df_dataset_original[,apply(df_dataset_original, 2, function(x) !any(is.na(x)))]
-  
-  
+#Perform a AGNES clusterization
+hclust.output<-function(df_dataset_original,linkagefun,distance,k,normalize){
+  df_dataset<-preProcessData(df_dataset_original,normalize)
   model_hclust<-hclust(dist(df_dataset,method = distance),method = linkagefun)
   df_output<-data.frame(df_dataset_original,Cluster=cutree(model_hclust,k=k))
   list_output<-list(df_output,model_hclust)
@@ -51,7 +57,80 @@ hclust.output<-function(df_dataset_original,linkagefun,distance,k){
   return(list_output)
 }
 
+
+#Plot Dendrogram
 hclust.plot<-function(list_output){
   plot(list_output$model)
   return(invisible())
 }
+
+
+#Summary
+hclust.summary<-function(list_output){
+  return(summary(list_output$model))
+}
+
+
+# DBSCAN ------------------------------------------------------------------
+
+#Perform a DBSCAN clusterization
+dbscan.output<-function(df_dataset_original,eps,minpts,normalize){
+  df_dataset<-preProcessData(df_dataset_original,normalize)
+  model_dbscan<-dbscan(df_dataset,eps = eps,minPts = minpts)
+  df_output<-data.frame(df_dataset_original,df_dataset,Cluster=model_dbscan$cluster)
+  list_output<-list(df_output,df_dataset,model_dbscan$cluster,model_dbscan)
+  names(list_output)<-c("output","input","labels","model")
+  return(list_output)
+}
+
+
+#Plot DBSCAN results cluster, if columns>2, then plot Principal Components
+dbscan.plot<-function(list_output){
+  if(ncol(list_output$input)>2){
+    list_output$input<-prcomp(list_output$input)$x[,1:2]
+  }
+  
+  #Index Array where noise is present
+  a_indnoise<-which(list_output$output$Cluster==0)
+  
+  plot(list_output$input,col=list_output$labels,pch=19)
+  points(list_output$input[a_indnoise,],col="grey",pch=4,cex=1.5)
+  return(invisible())
+}
+
+#Summary
+dbscan.summary<-function(list_output){
+  return(list_output$model)
+}
+
+
+
+# EMMG --------------------------------------------------------------------
+
+#Perform EMMG clusterization
+#Try simulations with an increment of +1 about gaussian mixture and then select simulation where log-likelihood is minimum
+emmg.output<-function(df_dataset_original,n,normalize){
+  df_dataset<-preProcessData(df_dataset_original,normalize)
+  model_emmg<-mclustBIC(df_dataset,G=2:n)
+  model_emmg_best<-Mclust(df_dataset,x=model_emmg)
+  df_output<-data.frame(df_dataset_original,df_dataset,Cluster=model_emmg_best$classification)
+  list_output<-list(df_output,df_dataset,model_emmg_best$classification,model_emmg_best)
+  names(list_output)<-c("output","input","labels","model")
+  return(list_output)
+}
+
+
+#Plot EMMG results cluster, if columns>2, then plot Principal Components
+emmg.plot<-function(list_output){
+  if(ncol(list_output$input)>2){
+    list_output$input<-prcomp(list_output$input)$x[,1:2]
+  }
+  plot(list_output$input,col=list_output$labels,pch=19)
+  return(invisible())
+}
+
+#Summary
+emmg.summary<-function(list_output){
+  return(summary(list_output$model))
+}
+
